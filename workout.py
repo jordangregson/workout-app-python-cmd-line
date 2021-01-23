115a51bb3590dc0af0f2a7d52544ffa8c81d8cef
Jordan Gregson <49405277+jordangregson@users.noreply.github.com>
2021-01-22 23:49:42 -0500

    Progress is being made
    The database is working properly, and all of the buttons are working the way they should, there are just some modifications that I do want to make, and eventually, I would like to give this app and GUI

--- workout.py
+++ workout.py
@@ -1,37 +1,64 @@
-def main():
+import sqlite3
+import sys
+import os
+os.system("clear")
+
+class WorkoutData:
+    def __init__(self, date, title, info):
+        self.date = date
+        self.title = title
+        self.info = info
+
+conn = sqlite3.connect('workout_data.db')
+c = conn.cursor()
+
+# c.execute("""CREATE TABLE workouts (
+#             date text,
+#             workouttype text,
+#             workoutinfo text
+#             )""")
+
+def insert_data():
+    with conn:
+        c.execute("INSERT INTO workouts VALUES ('{}', '{}', '{}')".format(date, workoutTitle, workoutInfo))
+
+def get_workouts_by_date(date):
     
-    import pickle
-    import os
-    import sys
+    c.execute("SELECT * FROM workouts WHERE date= :date", {'date': date})
+    return c.fetchall()
 
-    os.system("clear")
+def show_all_workouts():
+    with conn:
+        c.execute("SELECT * FROM workouts")
+        print(c.fetchall())
+
+
+
+addOrClose = input("What would you like to do:\n(0) Exit\n(1) Enter a new workout\n(2) Show all workouts\n(3) Search workouts by date\n\n")
+
+
+
+if(addOrClose == '0'):
+    sys.exit(0)  
+
+elif(addOrClose == '1'):
+    date = input("What is today's date (MM/DD/YYYY: ")
+    workoutTitle = input("What was the workout: ")
+    workoutInfo = input("What did you do during the workout: ")   
+    insert_data()
+
+elif(addOrClose == '2'):
+    show_all_workouts()
+
+
+elif(addOrClose =='3'):
+    search = input("Search date: ")
+    searchDate = get_workouts_by_date(search)
+    print(searchDate)
+    pass
 
     
-    
-
-    print("What would you like to do?\n\n0:Exit\n1:Enter new workout\n2:View Previous Workouts\n")
-    userSelect = input()
-    
-    if(userSelect == 0):
-        sys.exit
-
-    elif(userSelect == "1"):
-        date = input("Date: ")
-        e = input("Exercise: ")
-        d = input("Information about the exercise: ")
-
-        a = "data"
-        data = []
-        file = open(".dat", "wb")
-        data.append((date))
-        data.append((e))
-        data.append((d))
-
-        pickle.dump(data, file)
-
-        print(data)
-
-    #elif(userSelect == "2"):
 
 
-main()
+
+c.close()
